9fc1be2a427d13b8e79e17efb062d8ce54687db0
kananTakahashi <kanan.takahashi@hokusys.jp>
2019-07-22 17:54:35 +0900

    ローディング画面追加、募集一覧ページに通年採用メリット追加、各ページ修正、会社概要ページのメッセージ修正中

--- wp-content/themes/hokushinTheme_new/index.js
+++ wp-content/themes/hokushinTheme_new/index.js
@@ -1,4 +1,126 @@
 // ローディング画面
+
+(function($) {
+
+    function setimage() {
+
+        var $height = $(window).height();
+        var $width = $(window).width();
+        var win_ratio = $width / $height;
+
+        $(".Opening").css({
+            "height": $height
+        });
+        $(".Opening ul li").css({
+            "height": $height
+        });
+
+        if (win_ratio > 1.5) {
+
+            $(".Opening ul li img.Opening__img").css({
+                "width": "100%",
+                "height": "auto",
+                "margin-left": 0
+
+            });
+            var winwidth = $(".Opening").width();
+            var imgheight = winwidth * 0.66;
+            var winheight = $(".Opening").height();
+            var imgtopmargin = (imgheight - winheight) / 2;
+            $(".Opening ul li img.Opening__img").css({
+                "margin-top": -imgtopmargin
+            });
+        } else if (win_ratio < 1.5) {
+
+            var imgwidth = (($height / 800)) * 1200;
+            var winwidth = $(".Opening").width();
+            var imgmargin = (winwidth - imgwidth) / 2;
+            $(".Opening ul li img.Opening__img").css({
+                "width": imgwidth,
+                "height": $height,
+                "margin-left": imgmargin,
+                "margin-top": 0
+            });
+        }
+    }
+
+    //slide
+    $(function() {
+        setimage();
+        var $interval = 2000;
+        var $fade_speed = 2000;
+        $(".Opening ul li").hide().css({
+            "position": "absolute",
+            "top": 0,
+            // "top": 15,
+            "left": 0
+                // "left": 15
+        });
+        $(".Opening ul li:first").addClass("active").show();
+        $(".Opening ul li:first img.Opening__img").animate({
+            paddingRight: 0.1
+        }, {
+            duration: 5000,
+            step: function(now, fx) {
+                $(this).css({
+                    transform: 'scale(' + (1 + now) + ')',
+                    "top": 15,
+                });
+            },
+            complete: function() {
+                $(".Opening ul li img.Opening__img").css("paddingRight", 0);
+            }
+        })
+
+
+        var timer_id;
+        var i = 0;
+        timer_id = setInterval(function() {
+            console.log(i);
+            i++;
+            if (i == 2) {
+                clearInterval(timer_id);
+            } else {
+                var $active = $(".Opening ul li.active");
+                var $activeafter = $(".Opening ul li.active:after");
+                var $next = $active.next("li").length ? $active.next("li") : $(".Opening ul li:first");
+                //$active.fadeOut($fade_speed,function(){$active.removeClass("active");});
+                $active.fadeOut($fade_speed);
+                $active.removeClass("active");
+                $next.fadeIn($fade_speed).addClass("active");
+                $(".Opening ul li.active img.Opening__img").animate({
+                    paddingRight: 0.1
+                }, {
+                    duration: 5000,
+                    step: function(now, fx) {
+                        $(this).css({
+                            transform: 'scale(' + (1 + now) + ')'
+                        });
+                    },
+                    complete: function() {
+                        $(".Opening ul li img.Opening__img").css("paddingRight", 0);
+                    }
+                })
+            }
+
+        }, $interval);
+    });
+
+    //resize
+    var timer = false;
+    $(window).resize(function() {
+        if (timer !== false) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+            console.log('resized');
+            setimage();
+        }, 200);
+    });
+
+})(jQuery);
+
+
 var time = new Date().getTime();
 $(function() {
     var h = $(window).height();
@@ -20,8 +142,8 @@ $(function() {
 
 //4秒たったら強制的にロード画面を非表示
 $(function() {
-    setTimeout('imgTimer();', 2800);
-    setTimeout('stopload()', 3000);
+    setTimeout('imgTimer();', 3800);
+    setTimeout('stopload()', 4000);
 });
 
 // ロード画面を非表示にしてメイン画面と動きをスタートさせる
