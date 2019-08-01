4439dc9ec6793f123f6976422e025a5ff581ac49
kananTakahashi <kanan.takahashi@hokusys.jp>
2019-08-01 18:11:17 +0900

    各ページのコメント追加、トップページCSS整理、開発実績デザイン修正

--- wp-content/themes/hokushinTheme_new/common.js
+++ wp-content/themes/hokushinTheme_new/common.js
@@ -5,7 +5,7 @@ $('a[href^="#"]').click(function() {
     var speed = 400;
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top - 50;
+    var position = target.offset().top - 100;
     $('body,html').animate({ scrollTop: position }, speed, 'swing');
     return false;
 });
